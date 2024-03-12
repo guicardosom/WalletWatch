92f7fa04810e313d32154323a3665e26cedd004e
gccmiranda <gccardosomiranda@outlook.com>
2024-03-12 19:02:15 -0400

    Hides sensitive Firebase config data

--- FirebaseSetup.js
+++ FirebaseSetup.js
@@ -4,13 +4,13 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzPBhGahVNb1IKXrnsHqDgQroW2KNaWKA",
-  authDomain: "walletwatch-app.firebaseapp.com",
-  projectId: "walletwatch-app",
-  storageBucket: "walletwatch-app.appspot.com",
-  messagingSenderId: "329487416008",
-  appId: "1:329487416008:web:a97a96b75408d2711443fd",
-  measurementId: "G-L47BXTNCYC",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
