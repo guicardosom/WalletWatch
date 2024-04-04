1588d154bcd606b7ceaf099664fd636e90c27200
gccmiranda <gccardosomiranda@outlook.com>
2024-04-04 14:05:51 -0400

    Implements navigator for the newly created screens

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
+import DashboardScreen from "./src/screens/DashboardScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
 import { useFonts } from "expo-font";
 
 const Stack = createNativeStackNavigator();
@@ -27,6 +29,8 @@ export default App = () => {
       >
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
