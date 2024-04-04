e429c7a80690ce350cdc5a8b4fad1fd234baa32f
gccmiranda <gccardosomiranda@outlook.com>
2024-04-04 18:42:13 -0400

    Adds both AddTransaction and TransactionHistory to navigator

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import DashboardScreen from "./src/screens/DashboardScreen";
 import ProfileScreen from "./src/screens/ProfileScreen";
+import AddTransactionScreen from "./src/screens/AddTransactionScreen";
+import TransactionHistoryScreen from "./src/screens/TransactionHistoryScreen";
 import { useFonts } from "expo-font";
 
 const Stack = createNativeStackNavigator();
@@ -31,6 +33,11 @@ export default App = () => {
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="AddTransaction" component={AddTransactionScreen} />
+        <Stack.Screen
+          name="TransactionHistory"
+          component={TransactionHistoryScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
